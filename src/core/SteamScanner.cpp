4c4a558d145596304385bca7ee1554b7535a7467
mohammadmk47 <mohammedmk3900@gmail.com>
2026-06-03 15:49:02 +0330

    Update SteamScanner.cpp

--- src/core/SteamScanner.cpp
+++ src/core/SteamScanner.cpp
@@ -1,18 +1,24 @@
+#include "SteamScanner.h"
 #include <QDir>
 #include <QDebug>
+#include "Database.h"
 
-void scanSteamGames()
+void SteamScanner::scanSteamGames()
 {
-    QString steamPath =
-        "C:/Program Files (x86)/Steam/steamapps";
+    QString steamPath = "C:/Program Files (x86)/Steam/steamapps";
 
     QDir dir(steamPath);
-
-    QStringList manifests =
-        dir.entryList(QStringList() << "*.acf");
-
-    for (const QString& file : manifests)
-    {
-        qDebug() << file;
+    if (!dir.exists()) {
+        qDebug() << "Steam folder not found!";
+        return;
     }
+
+    QStringList manifests = dir.entryList(QStringList() << "*.acf", QDir::Files);
+
+    for (const QString& file : manifests) {
+        qDebug() << "Found manifest:" << file;
+        // بعداً اینجا parsing کامل acf فایل اضافه می‌کنیم
+    }
+
+    qDebug() << "Steam scan completed. Found" << manifests.size() << "games.";
 }
