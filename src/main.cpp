2fc306c3f58f0be025ae11c4f00a1969c86c2aed
mohammadmk47 <mohammedmk3900@gmail.com>
2026-06-03 15:51:13 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "core/Database.h"
+#include "core/SteamScanner.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    // Initialize Database
+    if (!Database::initialize()) {
+        qWarning() << "Failed to initialize database!";
+    }
+
+    // Scan Steam games on startup (اختیاری)
+    SteamScanner::scanSteamGames();
+
     QQmlApplicationEngine engine;
 
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection
-    );
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
 
     engine.loadFromModule("NeonLauncher", "Main");
 
