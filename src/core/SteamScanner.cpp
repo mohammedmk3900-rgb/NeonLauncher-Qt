35290cc0c25b456b056a6a8eb647d5e942859835
mohammadmk47 <mohammedmk3900@gmail.com>
2026-06-11 22:04:31 +0330

    Fix SteamScanner.cpp - add cross-platform support

--- src/core/SteamScanner.cpp
+++ src/core/SteamScanner.cpp
@@ -1,15 +1,27 @@
 #include "SteamScanner.h"
 #include <QDir>
 #include <QDebug>
+#include <QStandardPaths>
 #include "Database.h"
+
+QString SteamScanner::getSteamPath()
+{
+    #ifdef Q_OS_WIN
+        return "C:/Program Files (x86)/Steam/steamapps";
+    #elif defined(Q_OS_MAC)
+        return QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Library/Application Support/Steam/steamapps";
+    #else // Linux
+        return QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/.steam/steamapps";
+    #endif
+}
 
 void SteamScanner::scanSteamGames()
 {
-    QString steamPath = "C:/Program Files (x86)/Steam/steamapps";
+    QString steamPath = getSteamPath();
 
     QDir dir(steamPath);
     if (!dir.exists()) {
-        qDebug() << "Steam folder not found!";
+        qDebug() << "Steam folder not found at:" << steamPath;
         return;
     }
 
@@ -17,7 +29,8 @@ void SteamScanner::scanSteamGames()
 
     for (const QString& file : manifests) {
         qDebug() << "Found manifest:" << file;
-        // بعداً اینجا parsing کامل acf فایل اضافه می‌کنیم
+        // TODO: اینجا parsing کامل acf فایل اضافه می‌کنیم
+        // Database::addGame(gameName, gamePath, iconPath);
     }
 
     qDebug() << "Steam scan completed. Found" << manifests.size() << "games.";
