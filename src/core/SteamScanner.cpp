0ba5f6c16dc19b771a7a3def3e62439ad1b1d5f9
mohammadmk47 <mohammedmk3900@gmail.com>
2026-07-20 20:27:23 +0330

    Implement Steam manifest parsing and proper database integration

--- src/core/SteamScanner.cpp
+++ src/core/SteamScanner.cpp
@@ -2,6 +2,8 @@
 #include <QDir>
 #include <QDebug>
 #include <QStandardPaths>
+#include <QFile>
+#include <QTextStream>
 #include "Database.h"
 
 QString SteamScanner::getSteamPath()
@@ -15,6 +17,44 @@ QString SteamScanner::getSteamPath()
     #endif
 }
 
+QVector<GameInfo> SteamScanner::parseManifestFile(const QString& manifestPath)
+{
+    QVector<GameInfo> games;
+    QFile file(manifestPath);
+    
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Failed to open manifest:" << manifestPath;
+        return games;
+    }
+    
+    GameInfo game;
+    QTextStream in(&file);
+    
+    while (!in.atEnd()) {
+        QString line = in.readLine().trimmed();
+        
+        if (line.contains("\"name\"")) {
+            int start = line.indexOf("\"", line.indexOf("\"") + 1) + 1;
+            int end = line.lastIndexOf("\"");
+            game.name = line.mid(start, end - start);
+        }
+        else if (line.contains("\"installdir\"")) {
+            int start = line.indexOf("\"", line.indexOf("\"") + 1) + 1;
+            int end = line.lastIndexOf("\"");
+            game.exePath = line.mid(start, end - start);
+        }
+    }
+    
+    file.close();
+    
+    if (!game.name.isEmpty()) {
+        game.iconPath = "";  // Icon path would be determined from game directory
+        games.append(game);
+    }
+    
+    return games;
+}
+
 void SteamScanner::scanSteamGames()
 {
     QString steamPath = getSteamPath();
@@ -26,12 +66,21 @@ void SteamScanner::scanSteamGames()
     }
 
     QStringList manifests = dir.entryList(QStringList() << "*.acf", QDir::Files);
+    qDebug() << "Found" << manifests.size() << "manifest files";
 
+    int gamesAdded = 0;
     for (const QString& file : manifests) {
-        qDebug() << "Found manifest:" << file;
-        // TODO: اینجا parsing کامل acf فایل اضافه می‌کنیم
-        // Database::addGame(gameName, gamePath, iconPath);
+        QString fullPath = dir.filePath(file);
+        qDebug() << "Parsing manifest:" << file;
+        
+        QVector<GameInfo> parsedGames = parseManifestFile(fullPath);
+        
+        for (const GameInfo& game : parsedGames) {
+            Database::addGame(game.name, game.exePath, game.iconPath);
+            gamesAdded++;
+            qDebug() << "Added game to database:" << game.name;
+        }
     }
 
-    qDebug() << "Steam scan completed. Found" << manifests.size() << "games.";
+    qDebug() << "Steam scan completed. Added" << gamesAdded << "games to database.";
 }
