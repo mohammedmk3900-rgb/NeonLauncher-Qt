097018649c7e8d66ecf93a9625da658dd1ecd109
mohammadmk47 <mohammedmk3900@gmail.com>
2026-07-21 17:05:53 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "core/Database.h"
 #include "core/SteamScanner.h"
 #include "core/GameModel.h"
+#include "core/TranslationManager.h"    // ۱. اضافه کردن هدر مدیریت زبان
+#include "core/SaveBackupManager.h"    // ۲. اضافه کردن هدر مدیریت بکاپ سیو
 
 int main(int argc, char *argv[])
 {
@@ -27,10 +29,16 @@ int main(int argc, char *argv[])
     // Load games from database into model
     gameModel.loadGamesFromDatabase();
 
+    // ۳. مقداردهی اولیه سیستم مدیریت زبان و بکاپ
+    TranslationManager translationManager;
+    SaveBackupManager saveBackupManager;
+
     QQmlApplicationEngine engine;
 
-    // Expose gameModel to QML
+    // Expose objects to QML (۴. معرفی به محیط QML)
     engine.rootContext()->setContextProperty("gameModel", &gameModel);
+    engine.rootContext()->setContextProperty("trManager", &translationManager);
+    engine.rootContext()->setContextProperty("saveBackupManager", &saveBackupManager);
 
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
                      &app, []() { QCoreApplication::exit(-1); },
