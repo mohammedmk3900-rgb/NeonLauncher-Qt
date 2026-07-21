2d18a8cd2501596262e3179f8ac8cf41ba94ea07
mohammadmk47 <mohammedmk3900@gmail.com>
2026-07-21 15:29:21 +0330

    Update SteamScanner.cpp

--- src/core/SteamScanner.cpp
+++ src/core/SteamScanner.cpp
@@ -1,86 +1,45 @@
-#include "SteamScanner.h"
-#include <QDir>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QDebug>
-#include <QStandardPaths>
-#include <QFile>
-#include <QTextStream>
-#include "Database.h"
+#include "core/Database.h"
+#include "core/SteamScanner.h"
+#include "core/GameModel.h"
 
-QString SteamScanner::getSteamPath()
+int main(int argc, char *argv[])
 {
-    #ifdef Q_OS_WIN
-        return "C:/Program Files (x86)/Steam/steamapps";
-    #elif defined(Q_OS_MAC)
-        return QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/Library/Application Support/Steam/steamapps";
-    #else // Linux
-        return QStandardPaths::writableLocation(QStandardPaths::HomeLocation) + "/.steam/steamapps";
-    #endif
-}
+    QGuiApplication app(argc, argv);
 
-QVector<GameInfo> SteamScanner::parseManifestFile(const QString& manifestPath)
-{
-    QVector<GameInfo> games;
-    QFile file(manifestPath);
-    
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Failed to open manifest:" << manifestPath;
-        return games;
-    }
-    
-    GameInfo game;
-    QTextStream in(&file);
-    
-    while (!in.atEnd()) {
-        QString line = in.readLine().trimmed();
-        
-        if (line.contains("\"name\"")) {
-            int start = line.indexOf("\"", line.indexOf("\"") + 1) + 1;
-            int end = line.lastIndexOf("\"");
-            game.name = line.mid(start, end - start);
-        }
-        else if (line.contains("\"installdir\"")) {
-            int start = line.indexOf("\"", line.indexOf("\"") + 1) + 1;
-            int end = line.lastIndexOf("\"");
-            game.exePath = line.mid(start, end - start);
-        }
-    }
-    
-    file.close();
-    
-    if (!game.name.isEmpty()) {
-        game.iconPath = "";  // Icon path would be determined from game directory
-        games.append(game);
-    }
-    
-    return games;
-}
-
-void SteamScanner::scanSteamGames()
-{
-    QString steamPath = getSteamPath();
-
-    QDir dir(steamPath);
-    if (!dir.exists()) {
-        qDebug() << "Steam folder not found at:" << steamPath;
-        return;
+    // Initialize Database
+    if (!Database::initialize()) {
+        qWarning() << "Failed to initialize database!";
+        return -1;
+    } else {
+        qDebug() << "Database initialized successfully";
     }
 
-    QStringList manifests = dir.entryList(QStringList() << "*.acf", QDir::Files);
-    qDebug() << "Found" << manifests.size() << "manifest files";
+    // Create game model
+    GameModel gameModel;
 
-    int gamesAdded = 0;
-    for (const QString& file : manifests) {
-        QString fullPath = dir.filePath(file);
-        qDebug() << "Parsing manifest:" << file;
-        
-        QVector<GameInfo> parsedGames = parseManifestFile(fullPath);
-        
-        for (const GameInfo& game : parsedGames) {
-            Database::addGame(game.name, game.exePath, game.iconPath);
-            gamesAdded++;
-            qDebug() << "Added game to database:" << game.name;
-        }
-    }
+    // Scan Steam games on startup
+    SteamScanner::scanSteamGames();
 
-    qDebug() << "Steam scan completed. Added" << gamesAdded << "games to database.";
+    // Load games from database into model
+    gameModel.loadGamesFromDatabase();
+
+    QQmlApplicationEngine engine;
+
+    // Expose gameModel to QML
+    engine.rootContext()->setContextProperty("gameModel", &gameModel);
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
+
+    engine.loadFromModule("NeonLauncher", "Main");
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
