2184fbf5bd3c3888391e74847e62105852e243fe
mohammadmk47 <mohammedmk3900@gmail.com>
2026-07-21 15:29:48 +0330

    Update SteamScanner.h

--- src/core/SteamScanner.h
+++ src/core/SteamScanner.h
@@ -3,7 +3,6 @@
 
 #include <QString>
 #include <QVector>
-#include <QThread>
 #include <QObject>
 
 struct GameInfo {
@@ -12,51 +11,19 @@ struct GameInfo {
     QString iconPath;
 };
 
-class SteamScannerWorker : public QObject
-{
-    Q_OBJECT
-
-public:
-    explicit SteamScannerWorker(QObject *parent = nullptr);
-
-public slots:
-    void scanGames();
-
-signals:
-    void gameFound(const GameInfo &game);
-    void scanFinished(int totalGames);
-    void scanProgress(int current, int total);
-    void errorOccurred(const QString &error);
-
-private:
-    QString getSteamPath() const;
-    QVector<GameInfo> parseManifestFile(const QString& manifestPath);
-    void loadGameIcons(const GameInfo &game);
-    
-    int m_totalManifests = 0;
-};
-
 class SteamScanner : public QObject
 {
     Q_OBJECT
 
 public:
     explicit SteamScanner(QObject *parent = nullptr);
-    ~SteamScanner();
 
-    void startScanAsync();
-    void stopScan();
-    bool isScanning() const;
-
-signals:
-    void gameFound(const GameInfo &game);
-    void scanFinished(int totalGames);
-    void scanProgress(int current, int total);
-    void errorOccurred(const QString &error);
+    // تابع استاتیک برای اسکن راحت
+    static void scanSteamGames();
 
 private:
-    QThread *m_scanThread = nullptr;
-    SteamScannerWorker *m_worker = nullptr;
+    static QString getSteamPath();
+    static QVector<GameInfo> parseManifestFile(const QString& manifestPath);
 };
 
 #endif // STEAMSCANNER_H
