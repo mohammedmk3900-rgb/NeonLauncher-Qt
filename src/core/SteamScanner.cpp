4f15a3dd2947f24605f2886d87e674fd49d7ca51
mohammadmk47 <mohammedmk3900@gmail.com>
2026-07-21 16:09:45 +0330

    Update SteamScanner.cpp

--- src/core/SteamScanner.cpp
+++ src/core/SteamScanner.cpp
@@ -5,28 +5,16 @@
 
 void SteamScanner::scanSteamGames()
 {
-    // کدهای پیاده‌سازی اسکنر استیم شما
     qDebug() << "Scanning Steam games...";
 }
 
 QString SteamScanner::getSteamPath()
 {
-    // پیاده‌سازی مسیر استیم
     return "";
 }
 
 QVector<GameInfo> SteamScanner::parseManifestFile(const QString& manifestPath)
 {
     QVector<GameInfo> games;
-    // پیاده‌سازی پارس کردن فایل مانیفست
     return games;
-}
-                     Qt::QueuedConnection);
-
-    engine.loadFromModule("NeonLauncher", "Main");
-
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-    return app.exec();
 }
